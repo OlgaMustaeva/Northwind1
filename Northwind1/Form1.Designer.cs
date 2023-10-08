438fc6002661682ea02a98d71d7b7bbd24ffd856
wиллиs <ws@DESKTOP-6D0IVG5>
2023-10-08 17:47:18 +0500

    Добавьте файлы проекта.

---
+++ Northwind1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Northwind1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Customers = new System.Windows.Forms.Button();
+            this.Orders = new System.Windows.Forms.Button();
+            this.Products = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(76, 22);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(76, 60);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(796, 270);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Customers
+            // 
+            this.Customers.Location = new System.Drawing.Point(256, 22);
+            this.Customers.Name = "Customers";
+            this.Customers.Size = new System.Drawing.Size(75, 23);
+            this.Customers.TabIndex = 6;
+            this.Customers.Text = "Customers";
+            this.Customers.UseVisualStyleBackColor = true;
+            this.Customers.Click += new System.EventHandler(this.Customers_Click);
+            // 
+            // Orders
+            // 
+            this.Orders.Location = new System.Drawing.Point(405, 22);
+            this.Orders.Name = "Orders";
+            this.Orders.Size = new System.Drawing.Size(75, 23);
+            this.Orders.TabIndex = 7;
+            this.Orders.Text = "Orders";
+            this.Orders.UseVisualStyleBackColor = true;
+            this.Orders.Click += new System.EventHandler(this.Orders_Click);
+            // 
+            // Products
+            // 
+            this.Products.Location = new System.Drawing.Point(557, 23);
+            this.Products.Name = "Products";
+            this.Products.Size = new System.Drawing.Size(75, 23);
+            this.Products.TabIndex = 8;
+            this.Products.Text = "Products";
+            this.Products.UseVisualStyleBackColor = true;
+            this.Products.Click += new System.EventHandler(this.Products_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(256, 66);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 9;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(405, 66);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 10;
+            this.Save.Text = "Сохранить";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Products);
+            this.Controls.Add(this.Orders);
+            this.Controls.Add(this.Customers);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Customers;
+        private System.Windows.Forms.Button Orders;
+        private System.Windows.Forms.Button Products;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ Northwind1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Northwind1
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataAdapter adapter;
+        private DataTable CustomersTable = new DataTable("Customers");
+        private DataTable OrdersTable = new DataTable("Orders");
+        private DataTable ProductsTable = new DataTable("Products");
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            if (username == "Olga" && password == "111")
+            {
+                // Подключение к базе данных
+                SqlConnection connection = new SqlConnection("Data Source=DESKTOP-6D0IVG5\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Подключение успешно установлено.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при подключении к базе данных: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неправильное имя пользователя или пароль.");
+            }
+        }
+
+        private void Products_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Data Source=DESKTOP-6D0IVG5\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+            string query = "SELECT * FROM Products";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+               
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void Orders_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-6D0IVG5\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+            string query = "SELECT * FROM Orders";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+
+        }
+
+        private void Customers_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-6D0IVG5\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+            string query = "SELECT * FROM Customers";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                dataGridView1.Rows.RemoveAt(selectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку, которую хотите удалить.");
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.DataSource != null && adapter != null) 
+            {
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Update((DataTable)dataGridView1.DataSource);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
